ade81ae13f6c9e11cb5950e575e454b53b582414
Alexey Vidanov <vidanov@gmail.com>
2018-11-26 20:33:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -164,7 +164,7 @@ exports.handler = (event, context, callback) => {
 				console.log(data);	// successful response
 
 				// delete old backup
-				Lightsail.deleteInstance(params, function (err, data) {
+				Lightsail.DeleteInstanceSnapshot(params, function (err, data) {
 				if (err) {
 					// console.log(err, err.stack); // an error occurred
 				}
